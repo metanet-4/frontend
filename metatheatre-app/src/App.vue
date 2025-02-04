fe00bce305b33371bb4b5978e148c7dbec07ccc0
sje08250 <sje08250@gmail.com>
2025-02-04 16:30:05 +0900

    feat: create vue.js app

---
+++ metatheatre-app/src/App.vue
@@ -0,0 +1,15 @@
+<script setup></script>
+
+<template>
+  <div class="mobile-container"></div>
+</template>
+
+<style scoped>
+.mobile-container {
+  width: 100vw;
+  max-width: 390px;
+  height: 100vh;
+  margin: 0 auto;
+  overflow: hidden;
+}
+</style>
---
+++ metatheatre-app/src/main.js
@@ -0,0 +1,12 @@
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import "./style.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import i18n from "./i18n";
+
+const app = createApp(App);
+app.use(router);
+app.use(i18n);
+app.mount("#app");
