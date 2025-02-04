4e1245f5fb8abf039ad15cb40b3369562c9d6304
sje08250 <sje08250@gmail.com>
2025-02-04 17:53:59 +0900

    fix: Vue.js app

--- metatheatre-app/src/main.js
+++ metatheatre-app/src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-import router from "./router";
+import router from "./routers/index";
 import "./style.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
