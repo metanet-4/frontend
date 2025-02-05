175a501eb0d878377ece6d272c32c12d0a44d3b0
Son Jieun <116146923+sje08250@users.noreply.github.com>
2025-02-05 17:24:18 +0900

    Mn 18/feat/네이게이션 바 merge 및 좋아요 기능 추가 (#13)
    * 상세 정보 페이지 데이터 호출 및 유튜브 api 이용 예고편 호출 구현 완료
    
    * apexchart 추가 완료
    
    * Delete metatheatre-app/src/views/MovieTempPage.vue
    
    merge 완료2
    
    ---------
    
    Co-authored-by: parkjh0927 <dbapjh@gmail.com>
    Co-authored-by: parkjh0927 <101959377+parkjh0927@users.noreply.github.com>

--- metatheatre-app/src/routers/index.js
+++ metatheatre-app/src/routers/index.js
@@ -1,11 +1,17 @@
-import { createRouter, createWebHistory } from "vue-router";
-import MainPage from "../views/MainPage.vue";
+import { createRouter, createWebHistory } from 'vue-router';
+import MainPage from '../views/MainPage.vue';
+import MovieTempPage from '../views/MovieTempPage.vue';
+import MovieDetail from '../views/MovieDetail.vue'; // 상세 페이지 컴포넌트 추가
 
-const routes = [{ path: "/", component: MainPage }];
+const routes = [
+    { path: '/', component: MainPage },
+    { path: '/movie', component: MovieTempPage },
+    { path: '/movie/:movieId', component: MovieDetail, name: 'Detail' }, // 동적 라우트 추가
+];
 
 const router = createRouter({
-  history: createWebHistory(),
-  routes,
+    history: createWebHistory(),
+    routes,
 });
 
 export default router;
