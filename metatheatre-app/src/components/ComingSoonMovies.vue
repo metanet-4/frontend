942c27fdd7470e33a912ddb9a69a4a7942ebd743
sje08250 <sje08250@gmail.com>
2025-02-10 00:42:09 +0900

    feat: add file upload

--- metatheatre-app/src/components/ComingSoonMovies.vue
+++ metatheatre-app/src/components/ComingSoonMovies.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="csMovies">
     <p v-for="movie in csMovies" :key="movie.id">
-      {{ movie.krName }} - {{ movie.showTime }}분 - {{ movie.watchGrade }} ({{
-        movie.releaseDate
-      }}
-      개봉 예정)
+      <router-link :to="{ name: 'Detail', params: { movieId: movie.id } }"
+        >{{ movie.krName }} - {{ movie.showTime }}분 -
+        {{ movie.watchGrade }} ({{ movie.releaseDate }} 개봉)</router-link
+      >
     </p>
   </div>
 </template>
