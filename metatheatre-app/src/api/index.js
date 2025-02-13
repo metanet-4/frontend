caa880db58295a215d48b48a7d4310b80648a788
kokomong2 <ko9603@naver.com>
2025-02-13 12:04:07 +0900

    refactor: index.js baseurl 수정

--- metatheatre-app/src/api/index.js
+++ metatheatre-app/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8080",  // Spring Boot 백엔드 주소
+  baseURL: import.meta.env.VITE_API_BASE_URL,  // Spring Boot 백엔드 주소
   withCredentials: true, // JWT 토큰 쿠키를 주고받기 위해 필요
   headers: {
     "Content-Type": "application/json",
